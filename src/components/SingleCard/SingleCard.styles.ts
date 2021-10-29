77ebd70af690750b800c6ea068585e0da2758706
Jun Jung <juny929@gmail.com>
2021-10-29 13:53:49 +0300

    Creating a card component

--- src/components/SingleCard/SingleCard.styles.ts
+++ src/components/SingleCard/SingleCard.styles.ts
@@ -1,6 +1,14 @@
 import styled from '@emotion/styled';
 
-export const StyledSingleCard = styled.div({});
+export const StyledSingleCard = styled.div({
+  position: 'relative',
+  '& img': {
+    width: '100%',
+    display: 'block',
+    border: '2px solid #fff',
+    borderRadius: 6,
+  },
+});
 
 export const StyledFrontImg = styled.img({});
 
